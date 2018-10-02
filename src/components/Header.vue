5846b6942e26e477d5c02445e155fd2aed133429
newcoderlife <newcoderlife@outlook.com>
2018-10-02 22:10:58 +0800

    remove 'HelloWorld' component

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-container style="position: fixed">
+    <el-header>
+      Test Header
+    </el-header>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: "THWebHeader"
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
